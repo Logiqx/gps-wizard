f50b325a08b479d75a8962fe6f66b98fa9d0a9b9
Logiqx <software@mikeg.me.uk>
2022-05-15 14:15:20 +0100

    Add OAO reader

--- bin/docker_build.sh
+++ bin/docker_build.sh
@@ -12,6 +12,7 @@ DOCKER_BUILDKIT=1 docker build . --build-arg LOGIQX_DEBUG -t $IMAGE_NAME:$IMAGE_
 run_py_script common_core.py
 run_py_script readers/file_reader.py
 run_py_script readers/generic_reader.py
+run_py_script readers/oao_reader.py
 run_py_script readers/sirf_reader.py
 run_py_script readers/sbn_reader.py
 run_py_script readers/sbp_reader.py
